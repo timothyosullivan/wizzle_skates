7af286f4e475af4e06bb3daa4f4ab6ab1f73044c
Tim OSullivan <timothy.osullivan1@gmail.com>
2021-10-15 12:51:07 +0100

    Wizzle skates landing page - getting started

--- routes/index.js
+++ routes/index.js
@@ -1,8 +1,8 @@
 var express = require('express');
 var router = express.Router();
 
-let index = require('../controllers/index.js');
+let landing = require('../controllers/landing.js');
 /* GET home page. */
-router.get('/', index.index);
+router.get('/', landing.get_landing);
 
 module.exports = router;
