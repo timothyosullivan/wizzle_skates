0570cd49084c5a0de5ec51a1d95ccb2d571700c9
Tim OSullivan <timothy.osullivan1@gmail.com>
2021-10-15 13:02:14 +0100

    landing page - post route added

--- controllers/landing.js
+++ controllers/landing.js
@@ -2,3 +2,8 @@
 exports.get_landing = function(req, res, next) {
     res.render('landing', { title: 'Wizzle Skates' });
 }
+
+exports.submit_lead = function (req, res, next) {
+    console.log("Email submitted:" + req.body.lead_email);
+    res.redirect('/');
+}
--- routes/index.js
+++ routes/index.js
@@ -4,5 +4,6 @@ var router = express.Router();
 let landing = require('../controllers/landing.js');
 /* GET home page. */
 router.get('/', landing.get_landing);
+router.post('/', landing.submit_lead);
 
 module.exports = router;
